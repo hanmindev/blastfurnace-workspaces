d24c5ace602ca4b4fecb77796865d082b63c166e
Hanmin Kim <hanminkim123@gmail.com>
2024-07-22 23:54:34 -0700

    feat: pub everything

--- compiler/ast/src/ast.rs
+++ compiler/ast/src/ast.rs
@@ -1,3 +1,7 @@
+/// The AST module contains the definitions of the various types that make up the AST.
+///
+/// If you add anything here, you should look into updating the `visitor` module as well.
+
 use crate::ptr::P;
 use std::fmt::Debug;
 
@@ -17,20 +21,20 @@ pub struct Path {
 ///
 /// E.g., `foo` in `root::foo::Bar`
 #[derive(Debug, PartialEq, Clone)]
-struct PathSegment {
-    ident: Ident,
+pub struct PathSegment {
+    pub ident: Ident,
 }
 
 /// A definition (e.g., `struct`, `fn`)
 #[derive(Debug, PartialEq, Clone)]
-struct Definition<K = DefinitionKind> {
-    ident: Ident,
-    kind: K,
+pub struct Definition<K = DefinitionKind> {
+    pub ident: Ident,
+    pub kind: K,
 }
 
 /// The various kinds of definitions the compiler recognizes
 #[derive(Debug, PartialEq, Clone)]
-enum DefinitionKind {
+pub enum DefinitionKind {
     /// A struct definition (`struct`).
     /// E.g. `struct Foo { ... }`
     Struct(Struct),
@@ -58,30 +62,30 @@ pub enum TyKind {
 
 /// A struct definition
 #[derive(Debug, PartialEq, Clone)]
-struct Struct {
-    field: Vec<StructField>,
+pub struct Struct {
+    pub field: Vec<StructField>,
 }
 /// A struct field
 ///
 /// E.g., `foo: bar` in `struct Foo { foo: bar }`
 #[derive(Debug, PartialEq, Clone)]
-struct StructField {
-    ident: Ident,
-    ty: P<Ty>,
+pub struct StructField {
+    pub ident: Ident,
+    pub ty: P<Ty>,
 }
 
 /// A function definition
 #[derive(Debug, PartialEq, Clone)]
-struct Fn {
-    sig: FnSig,
-    body: Option<P<Block>>,
+pub struct Fn {
+    pub sig: FnSig,
+    pub body: Option<P<Block>>,
 }
 
 /// Represents a function's signature
 #[derive(Debug, PartialEq, Clone)]
-struct FnSig {
-    header: FnHeader,
-    decl: P<FnDecl>,
+pub struct FnSig {
+    pub header: FnHeader,
+    pub decl: P<FnDecl>,
 }
 
 /// A function header
@@ -89,36 +93,36 @@ struct FnSig {
 /// All the information that is not the visibility and the name of the function
 /// (e.g., `const rec fn`)
 #[derive(Debug, PartialEq, Clone)]
-struct FnHeader {
-    rec: bool,
-    constness: bool,
+pub struct FnHeader {
+    pub rec: bool,
+    pub constness: bool,
 }
 
 /// Signature of the function declaration
 /// E.g. fn foo(bar: baz) -> qux
 #[derive(Debug, PartialEq, Clone)]
-struct FnDecl {
-    inputs: Vec<Param>,
-    output: Ty,
+pub struct FnDecl {
+    pub inputs: Vec<Param>,
+    pub output: Ty,
 }
 
 /// A function parameter
 ///
 /// E.g. `bar: baz` in `fn foo(bar: baz) -> qux`
 #[derive(Debug, PartialEq, Clone)]
-struct Param {
-    ty: P<Ty>,
+pub struct Param {
+    pub ty: P<Ty>,
 }
 
 /// An expression
 #[derive(Debug, PartialEq, Clone)]
-struct Expr {
-    kind: ExprKind,
+pub struct Expr {
+    pub kind: ExprKind,
 }
 
 /// The various kinds of expressions the compiler recognizes
 #[derive(Debug, PartialEq, Clone)]
-enum ExprKind {
+pub enum ExprKind {
     Variable(Path),
     Constant(Constant),
     Block(Block),
@@ -126,14 +130,14 @@ enum ExprKind {
 
 /// A constant
 #[derive(Debug, PartialEq, Clone)]
-struct Constant {
-    kind: ConstantKind,
+pub struct Constant {
+    pub kind: ConstantKind,
 }
 
 /// The various kinds of constants the compiler recognizes
 /// E.g., `5`, `5.0`, `true`, `"foo"`
 #[derive(Debug, PartialEq, Clone)]
-enum ConstantKind {
+pub enum ConstantKind {
     Int(i64),
     Float(f64),
     Bool(bool),
@@ -142,14 +146,14 @@ enum ConstantKind {
 
 /// A local variable binding
 #[derive(Debug, PartialEq, Clone)]
-struct LocalBind {
-    ident: Ident,
-    ty: Option<Ty>,
-    kind: LocalBindKind,
+pub struct LocalBind {
+    pub ident: Ident,
+    pub ty: Option<Ty>,
+    pub kind: LocalBindKind,
 }
 
 #[derive(Debug, PartialEq, Clone)]
-enum LocalBindKind {
+pub enum LocalBindKind {
     /// Local variable binding
     /// E.g., `let x;`
     Decl,
@@ -160,13 +164,13 @@ enum LocalBindKind {
 
 /// A statement. E.g., `return;`
 #[derive(Debug, PartialEq, Clone)]
-struct Statement {
-    kind: StatementKind,
+pub struct Statement {
+    pub kind: StatementKind,
 }
 
 /// The various kinds of statements the compiler recognizes
 #[derive(Debug, PartialEq, Clone)]
-enum StatementKind {
+pub enum StatementKind {
     /// A local let binding
     Let(P<LocalBind>),
     /// A definition
@@ -179,6 +183,6 @@ enum StatementKind {
 ///
 /// E.g., fn foo() { ... }
 #[derive(Debug, PartialEq, Clone)]
-struct Block {
-    stmts: Vec<Statement>,
+pub struct Block {
+    pub stmts: Vec<Statement>,
 }
